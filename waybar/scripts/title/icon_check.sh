64cefdeabc68d1d7c022fda9eec3898415d592cd
sarath <sarathexp@gmail.com>
2023-01-15 08:40:09 +0530

    added hypr,waybar,swaylock configs

---
+++ waybar/scripts/title/icon_check.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+app_name=$(hyprctl activewindow | grep -oP 'class: \K[^\s]+')
+
+if [ -z "$app_name" ]; then
+  echo ""
+else
+  echo " "
+fi
+
+
+
+
---
+++ waybar/scripts/title/win_class_way.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+
+
+app_name=$(hyprctl activewindow | grep -oP 'class: \K[^\s]+')
+
+case "$app_name" in
+    "Code") echo "VSCode";;
+    "jetbrains-studio") echo "Android Studio";;
+    "Google-Chrome") echo "Chrome";;
+    "waybar") echo "hyprland";;
+    "jetbrains-idea") echo "Intellij";;
+    "uget-gtk") echo "Uget";;
+    "appimagekit_d2192f48ebc43a9db26e1dfa2bc5097b-Kotatogram_Desktop") echo "Telegram";;
+    "org.gnome.Nautilus") echo "File Manger";;
+    "Google-Chrome") echo "Chrome";;
+    *) echo "${app_name^}";;
+esac
+
+
+
