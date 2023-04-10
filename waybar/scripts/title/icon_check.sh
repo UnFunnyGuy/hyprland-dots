7c2fb1acbfa4722f84a4478c3e5cf93de64fb248
sarathEXP <sarathexp@gmail.com>
2023-04-10 18:53:53 +0530

    echoing separate app icons instead of the static one

--- waybar/scripts/title/icon_check.sh
+++ waybar/scripts/title/icon_check.sh
@@ -1,10 +1,33 @@
 #!/usr/bin/env bash
 #
 
+# Define the mapping between app names and symbols
+declare -A SYMBOLS=(
+  ["Code"]="󪥢"
+  ["Alacritty"]="󪥌"
+  ["jetbrains-studio"]="󪤍"
+  ["jetbrains-idea"]="󪤍"
+  ["uget-gtk"]="󪤐"
+  ["org.gnome.Nautilus"]="󪤈"
+  ["thunar"]="󪤈"
+  ["org.telegram.desktop"]="󪥣"
+  ["Google-Chrome"]="󪤕"
+  ["firefox"]="󪤕"
+)
+
+# Get the app name
 app_name=$(hyprctl activewindow | grep -oP 'class: \K[^\s]+')
 
+# Check if app_name is null
 if [ -z "$app_name" ]; then
   echo ""
 else
-  echo "󪥒"
+  # Retrieve the symbol from the mapping
+  symbol=${SYMBOLS[$app_name]}
+
+  # Echo the symbol if it's not empty
+  if [ -n "$symbol" ]; then
+    echo "$symbol"
+  fi
 fi
+
