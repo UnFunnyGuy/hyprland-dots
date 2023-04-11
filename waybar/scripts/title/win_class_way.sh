af57cce71edbc5049a995a68add0b288a2eaeef4
sarathEXP <sarathexp@gmail.com>
2023-04-11 19:02:34 +0530

    echo app class if its not declared

--- waybar/scripts/title/win_class_way.sh
+++ waybar/scripts/title/win_class_way.sh
@@ -11,14 +11,11 @@ declare -A PKGS=(
   ["thunar"]="File Manager"
   ["org.telegram.desktop"]="Telegram"
   ["Google-Chrome"]="Chrome"
+  ["firefox"]="Firefox"
 )
 
 app_name=$(hyprctl activewindow | grep -oP 'class: \K[^\s]+')
 
-# Check if app_name is null
-if [ -z "$app_name" ]; then
-  echo "${app_name}"
-else
 
  # Retrieve the PKS from the mapping
   pkg=${PKGS[$app_name]}
@@ -26,5 +23,6 @@ else
   # Echo the symbol if it's not empty
   if [ -n "$pkg" ]; then
     echo "$pkg"
-  fi
+  else
+  echo "$app_name"
 fi
