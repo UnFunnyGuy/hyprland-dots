43cf3937d0ed45922b0c297d4b06b5bef9347b28
UnFunnyGuy <sarathexp@gmail.com>
2023-06-06 09:43:47 +0530

    Font icon source set, allows anyone to add more icons to the set

---
+++ fontIcon/Madness_icons.dart
@@ -0,0 +1,115 @@
+// Place fonts/Madness.ttf in your fonts/ directory and
+// add the following to your pubspec.yaml
+// flutter:
+//   fonts:
+//    - family: Madness
+//      fonts:
+//       - asset: fonts/Madness.ttf
+import 'package:flutter/widgets.dart';
+
+class Madness {
+  Madness._();
+
+  static const String _fontFamily = 'Madness';
+
+  static const IconData wifi = IconData(0xea900, fontFamily: _fontFamily);
+  static const IconData speaker_3 = IconData(0xea901, fontFamily: _fontFamily);
+  static const IconData smart_home = IconData(0xea902, fontFamily: _fontFamily);
+  static const IconData note_square = IconData(0xea903, fontFamily: _fontFamily);
+  static const IconData message_programming = IconData(0xea904, fontFamily: _fontFamily);
+  static const IconData home_wifi = IconData(0xea905, fontFamily: _fontFamily);
+  static const IconData home = IconData(0xea906, fontFamily: _fontFamily);
+  static const IconData folder_cloud = IconData(0xea907, fontFamily: _fontFamily);
+  static const IconData folder_2 = IconData(0xea908, fontFamily: _fontFamily);
+  static const IconData emoji_happy = IconData(0xea909, fontFamily: _fontFamily);
+  static const IconData electricity = IconData(0xea90a, fontFamily: _fontFamily);
+  static const IconData document_code_2 = IconData(0xea90b, fontFamily: _fontFamily);
+  static const IconData danger = IconData(0xea90c, fontFamily: _fontFamily);
+  static const IconData code = IconData(0xea90d, fontFamily: _fontFamily);
+  static const IconData cloud_add = IconData(0xea90e, fontFamily: _fontFamily);
+  static const IconData audio_square = IconData(0xea90f, fontFamily: _fontFamily);
+  static const IconData arrange_square_2 = IconData(0xea910, fontFamily: _fontFamily);
+  static const IconData volume_slash = IconData(0xea911, fontFamily: _fontFamily);
+  static const IconData volume_high = IconData(0xea912, fontFamily: _fontFamily);
+  static const IconData flash_slash = IconData(0xea913, fontFamily: _fontFamily);
+  static const IconData cpu = IconData(0xea914, fontFamily: _fontFamily);
+  static const IconData chrome = IconData(0xea915, fontFamily: _fontFamily);
+  static const IconData Arrow___Right_2 = IconData(0xea916, fontFamily: _fontFamily);
+  static const IconData Arrow___Left_2 = IconData(0xea917, fontFamily: _fontFamily);
+  static const IconData toggle_on_circle = IconData(0xea918, fontFamily: _fontFamily);
+  static const IconData toggle_on = IconData(0xea919, fontFamily: _fontFamily);
+  static const IconData toggle_off_circle = IconData(0xea91a, fontFamily: _fontFamily);
+  static const IconData toggle_off = IconData(0xea91b, fontFamily: _fontFamily);
+  static const IconData shield_tick = IconData(0xea91c, fontFamily: _fontFamily);
+  static const IconData setting_3 = IconData(0xea91d, fontFamily: _fontFamily);
+  static const IconData search_normal = IconData(0xea91e, fontFamily: _fontFamily);
+  static const IconData ram_2 = IconData(0xea91f, fontFamily: _fontFamily);
+  static const IconData main_component = IconData(0xea920, fontFamily: _fontFamily);
+  static const IconData hashtag_up = IconData(0xea921, fontFamily: _fontFamily);
+  static const IconData driver = IconData(0xea922, fontFamily: _fontFamily);
+  static const IconData category = IconData(0xea923, fontFamily: _fontFamily);
+  static const IconData bucket_square = IconData(0xea924, fontFamily: _fontFamily);
+  static const IconData airpods = IconData(0xea925, fontFamily: _fontFamily);
+  static const IconData volume_low = IconData(0xea926, fontFamily: _fontFamily);
+  static const IconData video = IconData(0xea927, fontFamily: _fontFamily);
+  static const IconData timer = IconData(0xea928, fontFamily: _fontFamily);
+  static const IconData task_square = IconData(0xea929, fontFamily: _fontFamily);
+  static const IconData sun = IconData(0xea92a, fontFamily: _fontFamily);
+  static const IconData stop_circle = IconData(0xea92b, fontFamily: _fontFamily);
+  static const IconData speaker = IconData(0xea92c, fontFamily: _fontFamily);
+  static const IconData sms_star = IconData(0xea92d, fontFamily: _fontFamily);
+  static const IconData sms_notification = IconData(0xea92e, fontFamily: _fontFamily);
+  static const IconData sms = IconData(0xea92f, fontFamily: _fontFamily);
+  static const IconData record_circle = IconData(0xea930, fontFamily: _fontFamily);
+  static const IconData receive_square = IconData(0xea931, fontFamily: _fontFamily);
+  static const IconData ram = IconData(0xea932, fontFamily: _fontFamily);
+  static const IconData profile_tick = IconData(0xea933, fontFamily: _fontFamily);
+  static const IconData printer = IconData(0xea934, fontFamily: _fontFamily);
+  static const IconData previous = IconData(0xea935, fontFamily: _fontFamily);
+  static const IconData play_circle = IconData(0xea936, fontFamily: _fontFamily);
+  static const IconData pause_circle = IconData(0xea937, fontFamily: _fontFamily);
+  static const IconData notification = IconData(0xea938, fontFamily: _fontFamily);
+  static const IconData note_square1 = IconData(0xea939, fontFamily: _fontFamily);
+  static const IconData note_favorite = IconData(0xea93a, fontFamily: _fontFamily);
+  static const IconData next = IconData(0xea93b, fontFamily: _fontFamily);
+  static const IconData mouse = IconData(0xea93c, fontFamily: _fontFamily);
+  static const IconData moon = IconData(0xea93d, fontFamily: _fontFamily);
+  static const IconData monitor = IconData(0xea93e, fontFamily: _fontFamily);
+  static const IconData microphone_2 = IconData(0xea93f, fontFamily: _fontFamily);
+  static const IconData microphone = IconData(0xea940, fontFamily: _fontFamily);
+  static const IconData location = IconData(0xea941, fontFamily: _fontFamily);
+  static const IconData key = IconData(0xea942, fontFamily: _fontFamily);
+  static const IconData instagram = IconData(0xea943, fontFamily: _fontFamily);
+  static const IconData home_2 = IconData(0xea944, fontFamily: _fontFamily);
+  static const IconData hierarchy_square = IconData(0xea945, fontFamily: _fontFamily);
+  static const IconData headphone = IconData(0xea946, fontFamily: _fontFamily);
+  static const IconData forward = IconData(0xea947, fontFamily: _fontFamily);
+  static const IconData flash = IconData(0xea948, fontFamily: _fontFamily);
+  static const IconData driver_refresh = IconData(0xea949, fontFamily: _fontFamily);
+  static const IconData cpu_setting = IconData(0xea94a, fontFamily: _fontFamily);
+  static const IconData cpu_charge = IconData(0xea94b, fontFamily: _fontFamily);
+  static const IconData code1 = IconData(0xea94c, fontFamily: _fontFamily);
+  static const IconData cloud_sunny = IconData(0xea94d, fontFamily: _fontFamily);
+  static const IconData cloud_notif = IconData(0xea94e, fontFamily: _fontFamily);
+  static const IconData cloud = IconData(0xea94f, fontFamily: _fontFamily);
+  static const IconData clock = IconData(0xea950, fontFamily: _fontFamily);
+  static const IconData chart_2 = IconData(0xea951, fontFamily: _fontFamily);
+  static const IconData category1 = IconData(0xea952, fontFamily: _fontFamily);
+  static const IconData calendar_2 = IconData(0xea953, fontFamily: _fontFamily);
+  static const IconData box = IconData(0xea954, fontFamily: _fontFamily);
+  static const IconData bluetooth_2 = IconData(0xea955, fontFamily: _fontFamily);
+  static const IconData battery = IconData(0xea956, fontFamily: _fontFamily);
+  static const IconData battery_charging = IconData(0xea957, fontFamily: _fontFamily);
+  static const IconData backward = IconData(0xea958, fontFamily: _fontFamily);
+  static const IconData arrow_left = IconData(0xea959, fontFamily: _fontFamily);
+  static const IconData activity = IconData(0xea95a, fontFamily: _fontFamily);
+  static const IconData tick_circle = IconData(0xea95b, fontFamily: _fontFamily);
+  static const IconData arrow_right = IconData(0xea95c, fontFamily: _fontFamily);
+  static const IconData battery_10 = IconData(0xea95d, fontFamily: _fontFamily);
+  static const IconData battery_50 = IconData(0xea95e, fontFamily: _fontFamily);
+  static const IconData battery_full = IconData(0xea95f, fontFamily: _fontFamily);
+  static const IconData battery_disable = IconData(0xea960, fontFamily: _fontFamily);
+  static const IconData grid_2 = IconData(0xea961, fontFamily: _fontFamily);
+  static const IconData scroll = IconData(0xea962, fontFamily: _fontFamily);
+  static const IconData Send_fill_1 = IconData(0xea963, fontFamily: _fontFamily);
+}
---
+++ fontIcon/ie7/ie7.js
@@ -0,0 +1,131 @@
+/* To avoid CSS expressions while still supporting IE 7 and IE 6, use this script */
+/* The script tag referencing this file must be placed before the ending body tag. */
+
+/* Use conditional comments in order to target IE 7 and older:
+	<!--[if lt IE 8]><!-->
+	<script src="ie7/ie7.js"></script>
+	<!--<![endif]-->
+*/
+
+(function() {
+	function addIcon(el, entity) {
+		var html = el.innerHTML;
+		el.innerHTML = '<span style="font-family: \'Madness\'">' + entity + '</span>' + html;
+	}
+	var icons = {
+		'mad-wifi': '&#xea900;',
+		'mad-speaker-3': '&#xea901;',
+		'mad-smart-home': '&#xea902;',
+		'mad-note-square': '&#xea903;',
+		'mad-message-programming': '&#xea904;',
+		'mad-home-wifi': '&#xea905;',
+		'mad-home': '&#xea906;',
+		'mad-folder-cloud': '&#xea907;',
+		'mad-folder-2': '&#xea908;',
+		'mad-emoji-happy': '&#xea909;',
+		'mad-electricity': '&#xea90a;',
+		'mad-document-code-2': '&#xea90b;',
+		'mad-danger': '&#xea90c;',
+		'mad-code': '&#xea90d;',
+		'mad-cloud-add': '&#xea90e;',
+		'mad-audio-square': '&#xea90f;',
+		'mad-arrange-square-2': '&#xea910;',
+		'mad-volume-slash': '&#xea911;',
+		'mad-volume-high': '&#xea912;',
+		'mad-flash-slash': '&#xea913;',
+		'mad-cpu': '&#xea914;',
+		'mad-chrome': '&#xea915;',
+		'mad-Arrow---Right-2': '&#xea916;',
+		'mad-Arrow---Left-2': '&#xea917;',
+		'mad-toggle-on-circle': '&#xea918;',
+		'mad-toggle-on': '&#xea919;',
+		'mad-toggle-off-circle': '&#xea91a;',
+		'mad-toggle-off': '&#xea91b;',
+		'mad-shield-tick': '&#xea91c;',
+		'mad-setting-3': '&#xea91d;',
+		'mad-search-normal': '&#xea91e;',
+		'mad-ram-2': '&#xea91f;',
+		'mad-main-component': '&#xea920;',
+		'mad-hashtag-up': '&#xea921;',
+		'mad-driver': '&#xea922;',
+		'mad-category': '&#xea923;',
+		'mad-bucket-square': '&#xea924;',
+		'mad-airpods': '&#xea925;',
+		'mad-volume-low': '&#xea926;',
+		'mad-video': '&#xea927;',
+		'mad-timer': '&#xea928;',
+		'mad-task-square': '&#xea929;',
+		'mad-sun': '&#xea92a;',
+		'mad-stop-circle': '&#xea92b;',
+		'mad-speaker': '&#xea92c;',
+		'mad-sms-star': '&#xea92d;',
+		'mad-sms-notification': '&#xea92e;',
+		'mad-sms': '&#xea92f;',
+		'mad-record-circle': '&#xea930;',
+		'mad-receive-square': '&#xea931;',
+		'mad-ram': '&#xea932;',
+		'mad-profile-tick': '&#xea933;',
+		'mad-printer': '&#xea934;',
+		'mad-previous': '&#xea935;',
+		'mad-play-circle': '&#xea936;',
+		'mad-pause-circle': '&#xea937;',
+		'mad-notification': '&#xea938;',
+		'mad-note-square1': '&#xea939;',
+		'mad-note-favorite': '&#xea93a;',
+		'mad-next': '&#xea93b;',
+		'mad-mouse': '&#xea93c;',
+		'mad-moon': '&#xea93d;',
+		'mad-monitor': '&#xea93e;',
+		'mad-microphone-2': '&#xea93f;',
+		'mad-microphone': '&#xea940;',
+		'mad-location': '&#xea941;',
+		'mad-key': '&#xea942;',
+		'mad-instagram': '&#xea943;',
+		'mad-home-2': '&#xea944;',
+		'mad-hierarchy-square': '&#xea945;',
+		'mad-headphone': '&#xea946;',
+		'mad-forward': '&#xea947;',
+		'mad-flash': '&#xea948;',
+		'mad-driver-refresh': '&#xea949;',
+		'mad-cpu-setting': '&#xea94a;',
+		'mad-cpu-charge': '&#xea94b;',
+		'mad-code1': '&#xea94c;',
+		'mad-cloud-sunny': '&#xea94d;',
+		'mad-cloud-notif': '&#xea94e;',
+		'mad-cloud': '&#xea94f;',
+		'mad-clock': '&#xea950;',
+		'mad-chart-2': '&#xea951;',
+		'mad-category1': '&#xea952;',
+		'mad-calendar-2': '&#xea953;',
+		'mad-box': '&#xea954;',
+		'mad-bluetooth-2': '&#xea955;',
+		'mad-battery': '&#xea956;',
+		'mad-battery-charging': '&#xea957;',
+		'mad-backward': '&#xea958;',
+		'mad-arrow-left': '&#xea959;',
+		'mad-activity': '&#xea95a;',
+		'mad-tick-circle': '&#xea95b;',
+		'mad-arrow-right': '&#xea95c;',
+		'mad-battery-10': '&#xea95d;',
+		'mad-battery-50': '&#xea95e;',
+		'mad-battery-full': '&#xea95f;',
+		'mad-battery-disable': '&#xea960;',
+		'mad-grid-2': '&#xea961;',
+		'mad-scroll': '&#xea962;',
+		'mad-Send_fill-1': '&#xea963;',
+		'0': 0
+		},
+		els = document.getElementsByTagName('*'),
+		i, c, el;
+	for (i = 0; ; i += 1) {
+		el = els[i];
+		if(!el) {
+			break;
+		}
+		c = el.className;
+		c = c.match(/mad-[^\s'"]+/);
+		if (c && icons[c[0]]) {
+			addIcon(el, icons[c[0]]);
+		}
+	}
+}());
